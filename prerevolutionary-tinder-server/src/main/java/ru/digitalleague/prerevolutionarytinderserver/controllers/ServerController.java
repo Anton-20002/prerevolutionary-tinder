c3def27e263f47fc6cfcfad5b2fd0ea414785510
avchernikov <avchernikov@phoenixit.ru>
2023-04-27 21:17:03 +0300

    Change access modifier. v2.

--- prerevolutionary-tinder-server/src/main/java/ru/digitalleague/prerevolutionarytinderserver/controllers/ServerController.java
+++ prerevolutionary-tinder-server/src/main/java/ru/digitalleague/prerevolutionarytinderserver/controllers/ServerController.java
@@ -1,19 +1,24 @@
 package ru.digitalleague.prerevolutionarytinderserver.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.digitalleague.prerevolutionarytinderdatabase.entities.Person;
 import ru.digitalleague.prerevolutionarytinderdatabase.repositories.PersonRepository;
 
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping(value = "/person-attachment")
 public class ServerController {
 
     @Autowired
     PersonRepository personRepository;
 
-    public boolean isRegisteredPersonByChatId(long chatId) {
+    @GetMapping(value = "/is-registered-person-by-chat-id/{chatId}")
+    public boolean isRegisteredPersonByChatId(@PathVariable("chatId") Long chatId) {
         Optional<Person> personOptional = personRepository.findByChatId(chatId);
         return personOptional.isPresent();
     }
