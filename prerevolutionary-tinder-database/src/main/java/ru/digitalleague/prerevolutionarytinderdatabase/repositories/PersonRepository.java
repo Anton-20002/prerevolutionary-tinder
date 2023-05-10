d4250b4faa9b3cfdae4cd3a358074ef1d9b46469
avchernikov <avchernikov@phoenixit.ru>
2023-05-10 18:45:32 +0300

    Change query sql.

--- prerevolutionary-tinder-database/src/main/java/ru/digitalleague/prerevolutionarytinderdatabase/repositories/PersonRepository.java
+++ prerevolutionary-tinder-database/src/main/java/ru/digitalleague/prerevolutionarytinderdatabase/repositories/PersonRepository.java
@@ -31,6 +31,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Modifying
     @Query(value = "SELECT p.* FROM tinder.persons p " +
             "WHERE person_id NOT IN (SELECT person_id FROM tinder.favoritelist WHERE favorite_person_id = ?1) " +
+            "AND person_id NOT IN (SELECT favorite_person_id FROM tinder.favoritelist WHERE person_id = ?1) " +
             "AND person_id NOT IN (SELECT banned_person_id FROM tinder.blacklist WHERE person_id = ?1) " +
             "AND gender IN ?2 " +
             "AND orientation IN ?3",
