8e58f27c9668d7c3b7275cdf659dcf5d2649a2e6
Anton-20002 <35198993+Anton-20002@users.noreply.github.com>
2023-05-14 15:09:17 +0300

    Ревью

--- prerevolutionary-tinder-tgbot-client/src/main/java/ru/digitalleague/prerevolutionarytindertgbotclient/bot/service/FavoritesService.java
+++ prerevolutionary-tinder-tgbot-client/src/main/java/ru/digitalleague/prerevolutionarytindertgbotclient/bot/service/FavoritesService.java
@@ -28,9 +28,41 @@ public class FavoritesService {
     public List<ImageMessageDto> getFavorites(long chatId){
         List<FavoritePersonDto> favoritesList = dbService.getFavoritesByChatId(chatId);
         List<ImageMessageDto> imageMessageDtoList = new ArrayList<>();
+        /**
+        Перенести в блок цикла, иначе затираются записи
+        */
         ImageMessageDto imageMessageDto = new ImageMessageDto();
+        
         SendMessage sendMessage = new SendMessage();
-
+        
+        /**
+        Лучше обернуть в EXCEPTION
+        try {
+        List<FavoritePersonDto> favoritesList = dbService.getFavoritesByChatId(chatId);
+        if (favoritesList.isEmpty()) {
+        throw DataNotFoundEception("Отсутствуют избранные");}
+        
+        for (FavoritePersonDto favoritePersonDto : favoritesList) {
+            sendMessage.setChatId(chatId);
+            sendMessage.setText(favoritePersonDto.getRomanceStatus().getRussianName());
+            File picture = pictureService.createPicture(favoritePersonDto.getId(), favoritePersonDto.getImageFile());
+            imageMessageDto.setSendPhoto(pictureService.getSendPhoto(chatId, picture));
+            imageMessageDto.setSendMessage(sendMessage);
+            imageMessageDtoList.add(imageMessageDto);
+        }
+        
+        } catch (DataNotFoundException dnfe) {
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(dnfe.getMessage)
+        ...
+        } catch (PictureCreateException pce) {
+        sendMessage.setText(pce.getMessage)
+        }  catch (Exception e) {
+        log.error("Ошибка на стороне сервера", e)
+        sendMessage.setText("Ошибка на стороне сервера")
+        }
+        
+        */
         if (favoritesList.isEmpty()){
             sendMessage.setChatId(chatId);
             sendMessage.setText(messageService.getMessage("bot.command.menu.favorites.empty"));
@@ -40,6 +72,7 @@ public class FavoritesService {
         }
 
         for (FavoritePersonDto favoritePersonDto : favoritesList) {
+            // здесь нужно dto
             sendMessage.setChatId(chatId);
             sendMessage.setText(favoritePersonDto.getRomanceStatus().getRussianName());
             File picture = pictureService.createPicture(favoritePersonDto.getId(), favoritePersonDto.getImageFile());
